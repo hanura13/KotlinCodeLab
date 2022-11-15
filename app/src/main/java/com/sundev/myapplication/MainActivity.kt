8df4d02dd5deb3ea6fe7bf5e57d1014f234a5b80
hanura13 <hanuradiarjo@gmail.com>
2022-11-15 22:22:02 +0700

    Default Project

--- app/src/main/java/com/sundev/myapplication/MainActivity.kt
+++ app/src/main/java/com/sundev/myapplication/MainActivity.kt
@@ -9,29 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val diceButton: Button = findViewById(R.id.button)
-        diceButton.setOnClickListener {
-            rollDice()
-        }
-
-    }
-
-    private fun rollDice() {
-        val dice1 = Dice(6)
-        val diceroll = dice1.roll()
-        val textDice1: TextView = findViewById(R.id.textView)
-        textDice1.text = diceroll.toString()
-
-        val dice2 = Dice(6)
-        val diceroll2 = dice2.roll()
-        val textDice2: TextView = findViewById(R.id.textView2)
-        textDice2.text = diceroll2.toString()
     }
 }
 
-class Dice(val numSide: Int){
-    fun roll(): Int{
-        return (1..numSide).random()
-    }
-}
