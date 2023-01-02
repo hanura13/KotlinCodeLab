5c19d066ec868ffdeab99376b4904473121f380b
hanura13 <hanuradiarjo@gmail.com>
2023-01-02 14:30:52 +0700

    Init Cupcake Project

--- app/src/main/java/com/sundev/myapplication/MainActivity.kt
+++ app/src/main/java/com/sundev/myapplication/MainActivity.kt
@@ -5,10 +5,5 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
 
